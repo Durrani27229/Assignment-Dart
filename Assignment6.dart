fc70e4384577614d0b057da14d537f18bda2e6e0
Durrani27229 <u87262@gmail.com>
2025-08-08 07:01:01 -0700

    Assignment6

---
+++ Assignment6.dart
@@ -0,0 +1,91 @@
+import "dart:io";
+
+void main() {
+  // Q1. Write a program that prints the
+  // Fibonacci sequence up to a given number using a for loop.
+
+  // Example:
+  // Input:
+  // 10
+
+  // Output:
+  // 0 1 1 2 3 5 8
+
+  // Q2.  Implement a code that finds the
+  // largest element in a list using a for loop.
+
+  // Example:
+  // Input:
+  // [3, 9, 1, 6, 4, 2, 8, 5, 7]
+
+  // Output:
+  // Largest element: 9
+
+  // List nums = [3, 9, 1, 6, 4, 2, 8, 5, 7];
+  // int largest = nums[0];
+  // int smallest = nums[0];
+
+  // for (int i = 0; i < nums.length; i++) {
+  //   if (nums[i] > largest) {
+  //     largest = nums[i];
+  //   } else if (nums[i] < smallest) {
+  //     smallest = nums[i];
+  //   }
+  // }
+
+  // print(largest);
+  // print(smallest);
+
+  // Q3. Write a program that prints the
+  // multiplication table of a given number using a for loop.
+
+  // Example: Input: 5
+
+  // Output:
+  // 5 x 1= 5
+
+  // 5 x 2= 10
+
+  // 5 x 3= 15
+  // ...
+  // ...
+  // ...
+  // 5 x 10 = 50
+
+  // print("Enter a number: ");
+  // String? input = stdin.readLineSync();
+  // int number = int.parse(input!);
+  // // print(input);
+  // // print(number);
+
+  // for (int i = 1; i <= 10; i++) {
+  //   print("$number x $i = ${number * i}");
+  // }
+
+  //   Q4. Implement a function that checks if a given string is a
+  // palindrome.
+
+  // Example:
+
+  // Input:
+  // "radar"
+
+  // Output:
+  // "radar" is a palindrome.
+
+  // String? input = stdin.readLineSync();
+  // List<String> inputList = input!.split("");
+  // print(inputList);
+  // bool isPalindrome = true;
+  // for (int i = 0; i < inputList.length ~/ 2; i++) {
+  //   if (inputList[i] != inputList[inputList.length - 1 - i]) {
+  //     isPalindrome = false;
+  //     break;
+  //   }
+  // }
+  // if (isPalindrome) {
+  //   print("$input is a palindrome");
+  // } else {
+  //   print("$input is not a palindrome");
+  // }
+}
